599eab1ebe13ceb3c85187318296631346b87ced
xiongjie.xue <xiongjie.xue@lkcoffee.com>
2024-12-20 18:41:29 +0800

    feat(react): ✨ 新增react-demo

---
+++ packages/react/src/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { ConfigProvider } from 'antd';
+import type { Container } from 'react-dom/client';
+import App from './app';
+import './styles/reset.css';
+
+const container = document.querySelector('#root');
+const root = createRoot(container as Container);
+root.render(
+  <div>
+    <ConfigProvider>
+      <App />
+    </ConfigProvider>
+  </div>
+);
---
+++ packages/react/src/router/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { AntdLayout } from './antd-layout';
+
+import { createBrowserRouter } from 'react-router-dom';
+
+// import url from '../assets/plugin.svg?inline';
+import { getRoutes } from './extra-routes';
+
+export const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <AntdLayout />,
+      children: [
+        {
+          path: '1',
+          element: (
+            <div>
+              {/* 1111 <img src={url} width={200} /> */}
+            </div>
+          )
+        },
+        ...getRoutes(),
+      ]
+    }
+  ],
+  {
+    basename: '/'
+  }
+);
